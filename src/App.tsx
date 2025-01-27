3b851a8f6b6eb957a34fadd61ea51626a91e299b
alexfcode <alexf@tut.by>
2025-01-27 21:27:16 +0300

    lesson 1 is finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
 import './App.css'
-import { TodoList } from './TodoList'
+import { TaskType, TodoList } from './TodoList'
 
 function App() {
   // DATA:
-const todolisTitle1 = "What to learn"
-const todolisTitle2 = "What to buy"
+  const todolisTitle1 = "What to learn"
+  const todolisTitle2 = "What to bye"
+  const todolisTitle3 = "What to do"
+  const tasks1: Array<TaskType> = [
+    { id: 1, title: "HTML", isDone: true },
+    { id: 2, title: "CSS", isDone: true },
+    { id: 3, title: "REACT", isDone: false },
+    { id: 3, title: "REACT", isDone: false }
+  ]
+  const tasks2: TaskType[] = [
+    { id: 4, title: "Milk", isDone: true },
+    { id: 5, title: "Bread", isDone: true },
+    { id: 6, title: "Chicken", isDone: false }
+  ]
+
+  const tasks3: TaskType[] = [
+  ]
 
   //UI:
   return (
     <div className="app">
-      <TodoList title={todolisTitle1}/>
-      <TodoList title={todolisTitle2}/>
+      <TodoList
+        title={todolisTitle1}
+        tasks={tasks1} />
+      <TodoList
+        title={todolisTitle2}
+        tasks={tasks2}
+      />
+      <TodoList
+        title={todolisTitle3}
+        tasks={tasks3}
+      />
     </div>
   )
 }
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,33 +1,50 @@
+import { Button } from "./Button"
+
 type TodoListPropsType = {
-    title: string
+  title: string
+  tasks: Array<TaskType>
 }
 
-export const TodoList = (props: TodoListPropsType) => {
-  return (
+export type TaskType = {
+  id: number
+  title: string
+  isDone: boolean
+}
+
+export const TodoList = ({ title, tasks }: TodoListPropsType) => {
+  // ({ title, tasks }: TodoListPropsType) - это деструктуризация. На лету вытянули из пропсов ключи и значения присвоили одноименным переменным
+  // это равносильно записям ниже:
+  // const {title: title, tasks: tasks} = props
+  // или
+  // const {title, tasks} = props
+
+  const tasksList = tasks.length === 0
+    ? <span>Your list is empty</span>
+    : <ul>
+      {tasks.map(taks => {
+        return (
+          <li>
+            <input type="checkbox" checked={taks.isDone} /> <span>{taks.title}</span>
+          </li>
+        )
+      })}
+    </ul>
+
+return (
     <div>
+      <div>
+        <h3>{title}</h3>
         <div>
-          <h3>{props.title}</h3>
-          <div>
-            <input/>
-            <button>+</button>
-          </div>
-          <ul>
-            <li>
-              <input type="checkbox" checked={true}/> <span>HTML&CSS</span>
-            </li>
-            <li>
-              <input type="checkbox" checked={true}/> <span>JS</span>
-            </li>
-            <li>
-              <input type="checkbox" checked={false}/> <span>React</span>
-            </li>
-          </ul>
-          <div>
-            <button>All</button>
-            <button>Active</button>
-            <button>Completed</button>
-          </div>
+          <input />
+          <button>+</button>
         </div>
+        {tasksList}
+        <div>
+          <Button title={"All"} />
+          <Button title="Active"/>
+          <Button title="Completed"/>
+        </div>
+      </div>
     </div>
   )
 }
