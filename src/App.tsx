19227cf6da2c05c7a749aa576ca047757119e2ab
alexf <alexf@tut.by>
2026-04-07 10:27:08 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Todolist from './Todolist'
+
+export const App = () => {
+  return (
+      <div className="app">
+        <Todolist/>
+        <Todolist/>
+        <Todolist/>
+      </div>
+  )
+}
